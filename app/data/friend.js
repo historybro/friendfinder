36b44cf13924a65788468c494a3d1aa25cffeaec
Sean Massingill <25423602+historybro@users.noreply.github.com>
2019-05-03 15:43:36 -0500

    push to test on heroku

--- app/data/friend.js
+++ app/data/friend.js
@@ -3,27 +3,27 @@ var friends = [
     {
         name: "Test1",
         photo: "https://mtg.design/i/fozy35",
-        scores: [1]
+        scores: [1,1]
     },
     {
         name: "Test2",
         photo: "http://gatherer.wizards.com/Handlers/Image.ashx?type=card&name=Pteramander",
-        scores: [2]
+        scores: [2,2]
     },
     {
         name: "Test3",
         photo: "https://usercontent2.hubstatic.com/14291319_f520.jpg",
-        scores: [3]
+        scores: [3,3]
     },
     {
         name: "Test4",
         photo: "https://6d4be195623157e28848-7697ece4918e0a73861de0eb37d08968.ssl.cf1.rackcdn.com/183219_200w.jpg",
-        scores: [4]
+        scores: [4,4]
     },
     {
         name: "Test5",
         photo: "https://i.imgur.com/xxqKsSA.jpg",
-        scores: [5]
+        scores: [5,5]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,25 @@
 var friends = require("../data/friend");
 
-module.exports = function(app) {
-    app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
         res.json(friends)
     });
 
-    app.post("/api/friends", function(req, res) {
-        console.log(req.body);
-        res.json(friends);
-    })
+    app.post("/api/friends", function (req, res) {
+        var maxDiff = 50;
+        var matchFriend;
+        var currentFriend;
+        friends.forEach(function (friend) {
+            var difference = 0;
+            for (var i = 0; i < friends.scores.length; i++) {
+                difference += Math.abs(friends.scores[i] - currentFriend.scores[i]);
+            }
+            if (difference < maxDiff) {
+                maxDiff = difference;
+                matchFriend = friend;
+            };
+        });
+        res.json(matchFriend);
+        friends.push(req.body);
+    });
 };
