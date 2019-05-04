ee4452e6e03232833e8d9755832b93785f591a32
Sean Massingill <25423602+historybro@users.noreply.github.com>
2019-05-04 10:05:05 -0500

    basic readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # friendfinder
+
+The object of this assignment was to use express in order to build a  "friend finder"
+
+You would fill out a survey and then be matched with the person who answered the closets to you from a server of other users.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,21 +6,25 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        var diff;
-        var top = 50;
-        var match;
         var currentUser = req.body
-        for(var i =0; i < friends.length; i++){
-            diff = 0;
-            for(var j = 0; j < friends[i].scores.length; j++){
-                diff += Math.abs(currentUser.scores[j] - friends[i].scores[j]);
-            }
-            if(diff < top){
-                top = diff;
-                match = friends[i]
-            }
-        };
+        var friendID = matchFriend(currentUser, friends)        
         friends.push(currentUser);
-        res.json(match);
+        res.json(friends[friendID]);        
     });
+    var ql = 2;
+    function matchFriend(input, data) {
+        var results=[0, 100];
+        for (var i = 0; i < data.length; i++){
+            var result = 0;
+            for (var j = 0; j < ql; j++) {
+                results += Math.abs(parseInt(input.scores[j])-parseInt(data[i].scores[j]));
+            };
+            if(results[1] > result) {
+                results[0]=i;
+                results[1]=result;
+            }
+            console.log(results[0]); 
+        };
+        return results[0];               
+    }
 };
